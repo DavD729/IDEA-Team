3e38c2aa45200dc49459e4a127a0ccbca3eb298c
Antonio Ramos <antonio.rr.3795@gmail.com>
2023-05-31 12:30:36 -0600

    Agregue la inicializacion de mis ventas para la base de datos

--- src/main/java/mx/uam/ayd/proyecto/TiendaAplicacion.java
+++ src/main/java/mx/uam/ayd/proyecto/TiendaAplicacion.java
@@ -1,5 +1,7 @@
 package mx.uam.ayd.proyecto;
 
+import java.time.LocalDate;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import mx.uam.ayd.proyecto.datos.CategoriaRepository;
 import mx.uam.ayd.proyecto.datos.PuestoRepository;
+import mx.uam.ayd.proyecto.datos.VentaRepository;
 import mx.uam.ayd.proyecto.negocio.modelo.Categoria;
 import mx.uam.ayd.proyecto.negocio.modelo.Puesto;
+import mx.uam.ayd.proyecto.negocio.modelo.Venta;
 import mx.uam.ayd.proyecto.presentacion.inicio.ControlInicio;
 
 @SpringBootApplication
@@ -29,6 +33,9 @@ public class TiendaAplicacion {
 	
 	@Autowired
 	PuestoRepository puestoRepository;
+	
+	@Autowired
+	VentaRepository ventaRepository;
 	
 	public static void main(String[] args) {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(TiendaAplicacion.class);
@@ -57,5 +64,27 @@ public class TiendaAplicacion {
 		Puesto puestoVendedor = new Puesto();
 		puestoVendedor.setNombre("Vendedor");
 		puestoRepository.save(puestoVendedor);
+		
+		//Inicia la inicialización de la BD para ventas
+		LocalDate Fecha = LocalDate.now();
+		
+		Venta venta1 = new Venta();
+		venta1.setDate(Fecha);
+		venta1.setTipoVenta(1);
+		venta1.setTotal(59.5);
+		ventaRepository.save(venta1);
+		
+		Venta venta2 = new Venta();
+		venta2.setDate(Fecha);
+		venta2.setTipoVenta(2);
+		venta2.setTotal(542.3);
+		ventaRepository.save(venta2);
+		
+		Venta venta3 = new Venta();
+		venta3.setDate(Fecha);
+		venta3.setTipoVenta(1);
+		venta3.setTotal(78.62);
+		ventaRepository.save(venta3);
+		//Finaliza la inicialización de la BD para ventas
 	}
 }
