efabff295cdbaa0310f8e37786a8617553c01408
Antonio Ramos <antonio.rr.3795@gmail.com>
2023-06-14 11:04:11 -0600

    Comente el codigo

--- src/main/java/mx/uam/ayd/proyecto/negocio/ServicioVenta.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/ServicioVenta.java
@@ -3,10 +3,8 @@ package mx.uam.ayd.proyecto.negocio;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import mx.uam.ayd.proyecto.datos.VentaRepository;
 import mx.uam.ayd.proyecto.negocio.modelo.Venta;
 
@@ -16,9 +14,16 @@ public class ServicioVenta {
 	
 	@Autowired 
 	private VentaRepository ventaRepository;
-
+	/**
+	*Recupera la Lista de las ventas del día.
+	*
+	* @param LocalDate date: fecha actual del sistema.
+	* 
+	* @return List<Venta>: Lista de ventas del dia.
+	**/
 	public List<Venta> recuperaVentas(LocalDate date) {
 		List<Venta> ventas = new ArrayList<>();
+		
 		ventas=(List<Venta>) ventaRepository.findByDate(date);
 		return ventas;
 	}
